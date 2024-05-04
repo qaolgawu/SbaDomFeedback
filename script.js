60cd9f5a8f4530c1281e57e40f3d4a9f22d80df4
Olga <qaolgawu@gmail.com>
2024-05-03 21:32:24 -0400

    third commit

--- script.js
+++ script.js
@@ -18,11 +18,11 @@ function enableValidation(obj) { // Validation function
                 input.classList.remove(formValidation.inputErrorClass);
                 input.nextElementSibling.classList.remove(formValidation.errorClass);
             } else if (input.validity.valueMissing) { // validation if empty field
-                errMsg.textContent = 'This field is required ';
+                errMsg.textContent = 'This field is required';
                 input.classList.add(formValidation.inputErrorClass);
                 input.nextElementSibling.classList.add(formValidation.errorClass);
             } else if (input.validity.tooLong || input.validity.tooShort) { // check the length of input
-                errMsg.textContent = `Should be from${input.minLength} to ${input.maxLength} symbols`;
+                errMsg.textContent = `Should be from ${input.minLength} to ${input.maxLength} symbols`;
                 input.classList.add(formValidation.inputErrorClass);
                 input.nextElementSibling.classList.add(formValidation.errorClass);
             } else { // if everything is good 
@@ -35,3 +35,28 @@ function enableValidation(obj) { // Validation function
         })
     })
 }
+
+
+const submitForm = (e) => { // function which collect information from a form 
+    e.preventDefault();
+    const form = e.target
+    const {name, email, message} = form.elements;
+    const obj = {
+        name: name.value,
+        email: email.value,
+        message: message.value
+    }
+    const elem = document.createElement('div') // create element for information
+    elem.innerHTML = `
+        <p>Your name: ${obj.name}</p>
+        <p>Your email: ${obj.email}</p>
+        <p>Your message: ${obj.message}</p>`
+    form.append(elem) // add information to the form
+    form.reset() // clear the form
+}
+
+enableValidation(formValidation) // evoke validation function
+
+form.addEventListener('submit', submitForm) // adding eventListener to the form 
+// submitting the form
+// run function submitForm
